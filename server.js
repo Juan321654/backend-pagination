3ad3a3ca46b09ab34f41a7390e185ae476e14c31
Juan Camacho <kmilokmacho@hotmail.com>
2021-06-17 20:56:06 -0400

    done

--- server.js
+++ server.js
@@ -1,72 +1,67 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const User = require("./users");
 
-const users = [
-  { id: 1, name: 'User 1'},
-  { id: 2, name: 'User 2'},
-  { id: 3, name: 'User 3'},
-  { id: 4, name: 'User 4'},
-  { id: 5, name: 'User 5'},
-  { id: 6, name: 'User 6'},
-  { id: 7, name: 'User 7'},
-  { id: 8, name: 'User 8'},
-  { id: 9, name: 'User 9'},
-  { id: 10, name: 'User 10'},
-]
-const posts = [
-  { id: 1, name: 'Post 1'},
-  { id: 2, name: 'Post 2'},
-  { id: 3, name: 'Post 3'},
-  { id: 4, name: 'Post 4'},
-  { id: 5, name: 'Post 5'},
-  { id: 6, name: 'Post 6'},
-  { id: 7, name: 'Post 7'},
-  { id: 8, name: 'Post 8'},
-  { id: 9, name: 'Post 9'},
-  { id: 10, name: 'Post 10'},
-]
+mongoose.connect("mongodb://localhost/pagination", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.once("open", async () => {
+  if ((await User.countDocuments().exec()) > 0) return;
 
-// http://localhost:3000/users?page=1&limit=5
+  Promise.all([
+    User.create({ name: "User 1" }),
+    User.create({ name: "User 2" }),
+    User.create({ name: "User 3" }),
+    User.create({ name: "User 4" }),
+    User.create({ name: "User 5" }),
+    User.create({ name: "User 6" }),
+    User.create({ name: "User 7" }),
+    User.create({ name: "User 8" }),
+    User.create({ name: "User 9" }),
+    User.create({ name: "User 10" }),
+    User.create({ name: "User 11" }),
+    User.create({ name: "User 12" }),
+  ]).then(() => console.log("Added Users"));
+});
 
-app.get('/posts', paginatedResults(posts), (req, res) => {
-  res.json(res.paginatedResults)
-})
+app.get("/users", paginatedResults(User), (req, res) => {
+  res.json(res.paginatedResults);
+});
 
-app.get('/users', paginatedResults(users),(req, res) => {
-  res.json(res.paginatedResults)
-})
+function paginatedResults(model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
 
-function paginatedResults(model){
-  return (req, res, next) => {
-    const page = +req.query.page
-    const limit = +req.query.limit
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
 
-    const startIndex = (page - 1 ) * limit
-    const endIndex = (page * limit)
+    const results = {};
 
-    const results = {}
-
-    if (endIndex < model.length) {
+    if (endIndex < (await model.countDocuments().exec())) {
       results.next = {
         page: page + 1,
-        limit: limit
-      }
+        limit: limit,
+      };
     }
-    
+
     if (startIndex > 0) {
       results.previous = {
         page: page - 1,
-        limit: limit
-      }
+        limit: limit,
+      };
     }
-
-    results.data = model.slice(startIndex, endIndex)
-
-    res.paginatedResults = results
-    // next will go into the next change of events which will be req, res in the route
-    next()
-  }
+    try {
+      results.results = await model.find().limit(limit).skip(startIndex).exec();
+      res.paginatedResults = results;
+      next();
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
+  };
 }
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(3000);
